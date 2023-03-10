38170f851b5304394923e2575ce0bef074096e3f
rusty595 <rusty595@users.noreply.github.com>
2023-03-10 17:00:02 +0000

    js test refactor

---
+++ moment.js
@@ -0,0 +1,15 @@
+function BuildMoment(dburl)
+{
+  $.ajax({
+    url:dburl,
+    success: function (data){
+      var lines = data.split('\n');
+      var text = "";
+      for (var i = 0; i < lines.length; i++)
+        if (lines[i].length > 0)
+          text += "<blockquote class=\"twitter-tweet\" align=center data-conversation=none><img height=32 style=\"image-rendering: pixelated; vertical-align: middle;\" src=\"/img/loading.gif\"><a href=\"" + lines[i] + "\">" + lines[i] + "</a></blockquote>"
+      var el = document.getElementById('tweets'); 
+      el.innerHTML = text;
+    }
+  });
+}
