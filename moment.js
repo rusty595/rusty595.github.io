831163e79ff39ce57d5b72826a6dcdd429ef2db2
rusty595 <rusty595@users.noreply.github.com>
2023-10-08 11:07:57 +0100

    Revert "potentially fix tweet alignment"
    This reverts commit ef7a21b6ab27fd5e7103cc3f2cde2ab881838459.

--- moment.js
+++ moment.js
@@ -7,7 +7,7 @@ function BuildMoment(dburl)
       var text = "";
       for (var i = 0; i < lines.length; i++)
         if (lines[i].length > 0)
-          text += "<blockquote class=\"twitter-tweet tw-align-center\" align=center data-conversation=none><img height=32 style=\"image-rendering: pixelated; vertical-align: middle;\" src=\"/img/loading.gif\"><a href=\"" + lines[i] + "\">" + lines[i] + "</a></blockquote>"
+          text += "<blockquote class=\"twitter-tweet\" align=center data-conversation=none><img height=32 style=\"image-rendering: pixelated; vertical-align: middle;\" src=\"/img/loading.gif\"><a href=\"" + lines[i] + "\">" + lines[i] + "</a></blockquote>"
       var el = document.getElementById('tweets'); 
       el.innerHTML = text;
     }
