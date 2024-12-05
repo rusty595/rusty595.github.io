3bc06d7f0b39fb223e2703eb79562dfc980997b5
rusty595 <rusty595@users.noreply.github.com>
2024-12-05 11:58:34 +0000

    add bluesky embed generation to moment.js

--- moment.js
+++ moment.js
@@ -6,8 +6,20 @@ function BuildMoment(dburl)
       var lines = data.split('\n');
       var text = "";
       for (var i = 0; i < lines.length; i++)
+      {
         if (lines[i].length > 0)
-          text += "<blockquote class=\"twitter-tweet\" align=center data-conversation=none><img height=32 style=\"image-rendering: pixelated; vertical-align: middle;\" src=\"/img/loading.gif\"><a href=\"" + lines[i] + "\">" + lines[i] + "</a></blockquote>"
+        {
+          if (lines[i].includes("twitter.com")
+          {
+            text += "<blockquote class=\"twitter-tweet\" align=center data-conversation=none><img height=32 style=\"image-rendering: pixelated; vertical-align: middle;\" src=\"/img/loading.gif\"><a href=\"" + lines[i] + "\">" + lines[i] + "</a></blockquote>";
+          }
+          else if (lines[i].includes("bsky.app")
+          {
+            // unfortunately this hard codes my account's did:plc - which would be valid in the URL in place of my handle, but is not copied by default
+            text += "<blockquote class=\"bluesky-embed\" data-bluesky-uri=\"at://did:plc:wr33bahkchvqpn4lxpvagp6h/app.bsky.feed.post/" + lines[i].slice(lines[i].lastIndexOf("/")+1) + "\"><img height=32 style=\"image-rendering: pixelated; vertical-align: middle;\" src=\"/img/loading.gif\"><a href=\"" + lines[i] + "\">" + lines[i] + "</a></blockquote>";
+          }
+        }
+      }
       var el = document.getElementById('tweets'); 
       el.innerHTML = text;
     }
