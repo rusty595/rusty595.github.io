6031d6a9d16dc782b3e29f477d3162fbc811b89f
rusty595 <rusty595@users.noreply.github.com>
2024-12-05 12:35:12 +0000

    add centred div around bsky embeds

--- moment.js
+++ moment.js
@@ -16,7 +16,7 @@ function BuildMoment(dburl)
           else if (lines[i].includes("bsky.app"))
           {
             // unfortunately this hard codes my account's did:plc - which would be valid in the URL in place of my handle, but is not copied by default
-            text += "<blockquote class=\"bluesky-embed\" data-bluesky-uri=\"at://did:plc:wr33bahkchvqpn4lxpvagp6h/app.bsky.feed.post/" + lines[i].slice(lines[i].lastIndexOf("/")+1) + "\"><img height=32 style=\"image-rendering: pixelated; vertical-align: middle;\" src=\"/img/loading.gif\"><a href=\"" + lines[i] + "\">" + lines[i] + "</a></blockquote>";
+            text += "<div style=\"margin: auto\"><blockquote class=\"bluesky-embed\" data-bluesky-uri=\"at://did:plc:wr33bahkchvqpn4lxpvagp6h/app.bsky.feed.post/" + lines[i].slice(lines[i].lastIndexOf("/")+1) + "\"><img height=32 style=\"image-rendering: pixelated; vertical-align: middle;\" src=\"/img/loading.gif\"><a href=\"" + lines[i] + "\">" + lines[i] + "</a></blockquote></div>";
           }
         }
       }
